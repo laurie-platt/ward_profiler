ba5ecee976c5633bdb7e807f14bf9d56617e381b
laurie-platt <laurieplatt@hotmail.com>
2019-02-23 14:35:16 +0000

    trafford to sheffield pre may 2016 wards

--- data/economy/code/number_of_jobs.R
+++ data/economy/code/number_of_jobs.R
@@ -6,7 +6,7 @@
 
 library(tidyverse)
 
-df <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_190_1.data.csv?geography=E05000819...E05000839&date=latest&public_private=0&employment_status=1&measures=20100&select=date_name,geography_name,geography_code,public_private_name,employment_status_name,measures_name,obs_value,obs_status_name") %>%
+df <- read_csv("http://www.nomisweb.co.uk/api/v01/dataset/NM_190_1.data.csv?geography=E05001039...E05001066&date=latest&public_private=0&employment_status=1&measures=20100&select=date_name,geography_name,geography_code,public_private_name,employment_status_name,measures_name,obs_value,obs_status_name") %>%
   mutate(period = "2017",
          indicator = "Number of jobs",
          measure = "Count",
